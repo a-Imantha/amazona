c68494aa3f0830dedf9dd5732840eee693111b49
a-Imantha <imantha.ahangama@gmail.com>
2021-04-15 02:52:46 +0530

    Adding the Frontend React

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Nike Slim Shirt - M',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High Quality Slim Shirt'
+        },
+        {
+            _id:'2',
+            name: 'Nolimit Slim Shirt - M',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 150,
+            brand:'NoLimit',
+            rating: 4.0,
+            numReviews: 15,
+            description: 'High Quality Slim Shirt'
+        },
+        {
+            _id:'3',
+            name: 'Nike Slim Trouser - M',
+            category: 'Trousers',
+            image: '/images/p4.jpg',
+            price: 190,
+            brand:'Nike',
+            rating: 3.5,
+            numReviews: 50,
+            description: 'High Quality Slim Trouser'
+        },
+        {
+            _id:'4',
+            name: 'NoLimit Slim Trouser - M',
+            category: 'Trousers',
+            image: '/images/p5.jpg',
+            price: 170,
+            brand:'NoLimit',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'High Quality Slim Trousers'
+        },
+        {
+            _id:'5',
+            name: 'Adidas Free Shirt - M',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 125,
+            brand:'Adidas',
+            rating: 4.8,
+            numReviews: 40,
+            description: 'High Quality Free Shirt'
+        },
+        {
+            _id:'6',
+            name: 'Adidas Grey Trouser - M',
+            category: 'Trousers',
+            image: '/images/p6.jpg',
+            price: 180,
+            brand:'Adidas',
+            rating: 5.0,
+            numReviews: 40,
+            description: 'High Quality Grey Trouser'
+        }
+    ]
+};
+export default data;
